0f1a069b760175e17a33ffa0cc08ee2d32eb1892
Damian <justice.damian96@gmail.com>
2021-07-22 05:18:15 +1000

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const messages = []
+    for (let name of names) {
+        messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
